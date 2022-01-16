f193d1f49c54217070e43749844f23b372bf908f
Chris Moore <tonio64286@gmail.com>
2022-01-15 23:34:40 -0500

    Final

--- Strategy/BubbleSort.java
+++ Strategy/BubbleSort.java
@@ -17,10 +17,10 @@ public class BubbleSort implements SortBehavior {
       String temp = "";
       for (int i = 0; i < length; i++) {
         for (int j = i + 1; j < length; j++) {
-          if(data.get(i).compareTo(data.get(i + 1)) > 0) {
+          if(data.get(i).compareTo(data.get(j)) > 0) {
             temp = data.get(i);
-            data.set(i, data.get(i + 1));
-            data.set(i + 1, temp);
+            data.set(i, data.get(j));
+            data.set(j, temp);
           }
         }
       }
